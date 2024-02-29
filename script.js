9c004fc68076572cde4359e03e4838b292c71f30
Kamila <kamilanawrat@gmail.com>
2024-02-29 16:33:31 +0100

    main variables catched

---
+++ script.js
@@ -0,0 +1,7 @@
+'use-strict';
+
+const textArea = document.querySelector('.text-input');
+const speedInput = document.querySelector('.speed');
+const playBtn = document.querySelector('.play');
+const pauseBtn = document.querySelector('.pause');
+const stopBtn = document.querySelector('.stop');
